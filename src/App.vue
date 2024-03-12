8051ee9edbc8fcf9a09ce3b5256579d3c3953d0e
zhangyu <1015371461@qq.com>
2024-03-12 11:15:59 +0800

    二次封装el-input

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <MyInput :a="1" :b="2" :c="3" v-model="user.name" @click="add" ref="inputRef">
+    <template #prefix>
+      <span>aaa</span>
+    </template>
+    <template #suffix>
+      <span>bbb</span>
+    </template>
+    <template #prepend>
+      <span>ccc</span>
+    </template>
+    <template #append>
+      <span>ddd</span>
+    </template>
+  </MyInput>
+</template>
+
+<script setup>
+import MyInput from '@/MyInput.vue'
+import { reactive, ref, onMounted } from 'vue'
+
+const user = reactive({ name: 'John', age: 18, grade: 98 })
+
+const add = () => {
+  user.age++
+  console.log(user.age)
+}
+
+const inputRef = ref(null)
+// inputRef.value.elInp.sum()
+
+onMounted(() => {
+  console.log('333', inputRef.value.elInp)
+})
+</script>
+
+<style scoped></style>
---
+++ src/MyInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="myinput">
+    <el-input ref="elInp" v-bind="attrs" @click="sum">
+      <template v-for="(value, name) in slots" #[name]="scopedData">
+        <slot :name="name"> </slot>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, useAttrs, useSlots, ref } from 'vue'
+
+const attrs = useAttrs()
+const slots = useSlots()
+const elInp = ref(null)
+defineExpose({
+  elInp
+})
+
+defineProps({
+  a: Number,
+  b: Number
+})
+
+onMounted(() => {
+  // 直接访问 $attrs，无需通过 this
+  console.log(attrs)
+  console.log(slots)
+})
+
+const sum = () => {
+  console.log('hello')
+}
+</script>
+
+<style scoped>
+.myinput {
+  background-color: aqua;
+  border: 10px solid red;
+}
+</style>
